9a9c4c97c74ebc8d1df0270b1d878118e9511175
DerekWestbrooke <adegtyarev999@gmail.com>
2025-07-14 12:11:37 +0300

    Connection qss to the GUI

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 import sys
+import os
 
 from PyQt5.QtWidgets import QApplication
 from calculator import Calculator
+from PyQt5.QtCore import QFile, QTextStream
+
 
 if __name__ == '__main__':
+    print(os.getcwd() + '/widget_styles.qss')
     app = QApplication(sys.argv)
+    file = QFile(os.getcwd() + '/widget_styles.qss')
+    if file.open(QFile.ReadOnly | QFile.Text):
+        stream = QTextStream(file)
+        qss = stream.readAll()
+        app.setStyleSheet(qss)
+    else:
+        print("nooo")
+
     calc = Calculator()
     calc.show()
     sys.exit(app.exec_())
